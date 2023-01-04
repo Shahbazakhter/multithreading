cbc696917a4d0f2c65dc10c0f09e771b3870438b
Shahbaz Akhter <akhter.shahbaz31@gmail.com>
2023-01-04 22:04:04 +0530

    Added comment here

--- src/com/basic/producer/consumer/Consumer.java
+++ src/com/basic/producer/consumer/Consumer.java
@@ -4,6 +4,9 @@ import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.stream.IntStream;
 
+/**
+This is a consumer
+ */
 public class Consumer implements Runnable {
     private final Queue<Integer> queue;
 
